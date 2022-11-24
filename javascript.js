7ef82e0b58d5e6160a7cfd2e7b7db94d83478ab8
WoozyRavioli <110045994+WoozyRavioli@users.noreply.github.com>
2022-11-24 17:45:37 +0000

    Initiated getComputerChoice

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3)
+    switch(choice) {
+        case choice == 0:
+            return "Rock";
+        case choice == 1:
+            return "Paper";
+        case choice == 2:
+            return "Scissors";
+    }
+
+
+}

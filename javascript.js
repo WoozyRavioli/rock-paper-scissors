4ec44db776b2ae887f4b353a96b6b8c9d096b03c
WoozyRavioli <110045994+WoozyRavioli@users.noreply.github.com>
2022-11-24 21:03:15 +0000

    Play 5 rounds

--- javascript.js
+++ javascript.js
@@ -2,12 +2,44 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random()*3);
     
     if (choice==0) {
-    return "Rock"
+        return "Rock"
     } else if (choice==1){
-    return "Paper"
+        return "Paper"
     } else if (choice==2){
-    return "Scissors"
+        return "Scissors"
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    player = playerSelection.toLowerCase();
+    computer = computerSelection.toLocaleLowerCase();
 
+    if ((player == "rock" && computer == "scissors")||(player == "paper" && computer == "rock")||(player == "scissors" && computer == "paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+
+    } else if ((player == "rock" && computer == "paper")||(player == "paper" && computer == "scissors")||(player == "scissors" && computer == "rock")) {
+        return `You Lose! ${playerSelection} loses to ${computerSelection}`;
+
+    } else if ((player == "rock" && computer == "rock")||(player == "paper" && computer == "paper")||(player == "scissors" && computer == "scissors")) {
+        return `You Draw! ${playerSelection} equals to ${computerSelection}`;
+
+    } else {
+        return "Invalid input"
+    }
+
+}
+
+function game() {
+    let result = '';
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock paper scissors?");
+        const computerSelection = getComputerChoice();
+        result += playRound(playerSelection, computerSelection) + ". ";
+
+    }
+
+    return result;
+}
+
+console.log(game());
